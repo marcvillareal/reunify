c3d3810e2ff6098f04c7d5929aae609ab41b4ab2
marcvillareal <marcvillareal30@gmail.com>
2023-11-20 14:04:45 +0000

    Project setup - React, TailwindCSS, Vite

---
+++ reunify-project/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div></div>
+      <h1 className="font-bold">Reunify</h1>
+      <p className="read-the-docs">
+        In development
+      </p>
+    </>
+  );
+}
+
+export default App;
